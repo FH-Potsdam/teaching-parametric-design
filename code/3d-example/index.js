2a9f1ce47c3f58a5e8a20fc6b3db025621ff5466
Sebastian Meier <contact@sebastianmeier.eu>
2021-11-22 14:37:39 +0100

    more imports and boolean example

--- code/3d-example/index.js
+++ code/3d-example/index.js
@@ -1,7 +1,9 @@
 const jscad = require('@jscad/modeling')
-const { cube, cuboid, cylinder, cylinderElliptic, ellipsoid, geodesicSphere, roundedCuboid, roundedCylinder, sphere, torus } = jscad.primitives;
-const { union } = jscad.booleans;
-const { translate } = jscad.transforms;
+const {line, arc, circle, ellipse, rectangle, cube, sphere, cylinder, cuboid, roundedCuboid, geodesicSphere, ellipsoid, roundedCylinder, cylinderElliptic, torus} = jscad.primitives;
+const {extrudeRectangular, extrudeLinear, extrudeRotate} = jscad.extrusions;
+const {colorize, colorNameToRgb} = jscad.colors;
+const {union, subtract, intersect, scission} = jscad.booleans;
+const {translate, rotate, scale, center, align} = jscad.transforms;
 
 const main = () => {
   const allPrimitives = [
