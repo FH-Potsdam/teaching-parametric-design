550d2e3062ee15abe3cc476b21790f8036371738
Dimitri Balzer <38956494+balzerdi@users.noreply.github.com>
2021-11-24 10:57:13 +0100

    Task 4.02 - Circle added

--- code/task-04.2/sketch.js
+++ code/task-04.2/sketch.js
@@ -12,18 +12,23 @@ function draw() {
   const width = 150;
   const height = 150;
 
-  const limit = 256;
+  const limit = 256*4;
 
   for (let i = 0; i <= limit; i++) {
     push()
     translate(sketchWidth / 2, sketchHeight / 2);
     rotate(PI / limit * i);
-    fill(color(0+i*(250/limit)));
+    fill(color(0+i*(255/limit)));
     rect(0-width/2, 0-width/2, width, height);
     pop()  
   }
 
+  noFill()
+  stroke('white')
+  strokeWeight(2)
+  circle(200,200,213)
+
   noLoop();
   //un-comment to save
-  //save('sketch-04.2.png')
+  save('sketch-04.2.png')
 }
