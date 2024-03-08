1a44ee5ccecefbbc9ffc889246632fb018879d2a
Sebastian Meier <contact@sebastianmeier.eu>
2024-03-08 14:59:08 +0100

    clean main

--- examples/wise22/1130/html.js
+++
@@ -1,7 +0,0 @@
-const div = document.querySelectorAll('#test');
-div[0].style = "background-color:red;";
-
-for (let i = 0; i < 100; i += 1) {
-  const newDiv = div[0].cloneNode(true);
-  document.querySelector('#copycontainer').append(newDiv);
-}
