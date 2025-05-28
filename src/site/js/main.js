2ed60fc46a3295e0c28bcba808380dfc31d5d456
Sebastian Meier <contact@sebastianmeier.eu>
2025-05-28 14:03:46 +0200

    mobile menu fix

--- src/site/js/main.js
+++ src/site/js/main.js
@@ -4,3 +4,13 @@ for (let i = 0; i < buttons.length; i += 1) {
     window.scrollTo(0, window.scrollY + 100);
   });
 }
+
+document.querySelectorAll('#nav nav h5').forEach(header => {
+  header.addEventListener('click', function () {
+    const isOpen = this.classList.contains('open');
+    document.querySelectorAll('#nav nav h5').forEach(h => h.classList.remove('open'));
+    if (!isOpen) {
+      this.classList.add('open');
+    }
+  });
+});
